ee52d149626f2737bfec2dcb6e68fa3b31c28d16
ahzia <ah.ziayosfi@gmail.com>
2022-01-20 14:40:08 +0430

    create route for greetings#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :v`1, default: { format: 'json` } do
+    get 'greetings', to 'things#index'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
